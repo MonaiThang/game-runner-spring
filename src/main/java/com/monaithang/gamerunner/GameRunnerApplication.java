a188b714ba73641e0a90e50f877864ba23cf7937
Monai Thangsuphanich <mthangsuphanich@atlassian.com>
2023-02-03 11:22:46 +1100

    first commit

---
+++ src/main/java/com/monaithang/gamerunner/GameRunnerApplication.java
@@ -0,0 +1,13 @@
+package com.monaithang.gamerunner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameRunnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameRunnerApplication.class, args);
+	}
+
+}
